dbfa8b0aa7c58bbf81eda5da14ad10f67154395d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-02-11 23:58:46 +0000

    feat: add List component to fetch and display people data

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,8 +1,16 @@
 import "./Header.css";
 export function Header() {
-    return (
-        <header>
-            <h1>Lista osób</h1>
-        </header>
-    );
+  export const List = () => {
+    const { data: people } = useQuery({
+      queryFn: () =>
+        fetch("http://localhost:3000/people").then((res) => res.json()),
+      queryKey: ["people"],
+    });
+  };
+
+  return (
+    <header>
+      <h1>Lista osób</h1>
+    </header>
+  );
 }
