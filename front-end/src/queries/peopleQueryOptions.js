f007e8685797f833fedd68f1bb0731c3e0eedd2f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-02-12 00:29:31 +0000

    refactor: update peopleQueryOptions to use queryOptions from @tanstack/react-query

--- front-end/src/queries/peopleQueryOptions.js
+++ front-end/src/queries/peopleQueryOptions.js
@@ -1,5 +1,7 @@
-export const peopleQueryOptions = {
+import { queryOptions } from "@tanstack/react-query"; // makes working with TS easier
+
+export const peopleQueryOptions = queryOptions({
   queryFn: () =>
     fetch("http://localhost:3000/people").then((res) => res.json()),
   queryKey: ["people"],
-};
+});
