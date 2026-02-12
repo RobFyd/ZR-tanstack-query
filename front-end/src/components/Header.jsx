7048374b3b9aabb7e2804920d1a443577bf92146
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-02-12 00:03:20 +0000

    fix: update Header component to display the number of people fetched from API

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,16 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
 import "./Header.css";
+
 export function Header() {
-  export const List = () => {
-    const { data: people } = useQuery({
-      queryFn: () =>
-        fetch("http://localhost:3000/people").then((res) => res.json()),
-      queryKey: ["people"],
-    });
-  };
+  const { data: people } = useQuery({
+    queryFn: () =>
+      fetch("http://localhost:3000/people").then((res) => res.json()),
+    queryKey: ["people"],
+  });
 
   return (
     <header>
-      <h1>Lista osób</h1>
+      <h1>Lista osób {people && `${people.length}`}</h1>
     </header>
   );
 }
