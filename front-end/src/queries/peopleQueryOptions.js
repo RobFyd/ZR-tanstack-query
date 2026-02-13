e8163b79963126252d0bd0aa9e083fdca24599c2
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-02-13 01:50:58 +0000

    fix: update fetch URL in peopleQueryOptions to point to the correct server

--- front-end/src/queries/peopleQueryOptions.js
+++ front-end/src/queries/peopleQueryOptions.js
@@ -2,6 +2,8 @@ import { queryOptions } from "@tanstack/react-query"; // makes working with TS e
 
 export const peopleQueryOptions = queryOptions({
   queryFn: () =>
-    fetch("http://localhost:3000/people").then((res) => res.json()),
+    fetch("http://localhost:3001/people").then((res) => res.json()),
   queryKey: ["people"],
+  retry: 5,
+  retryDelay: 1000,
 });
