926ea79a632f5c35aeb00a3146cffe57a8ff8939
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-02-28 01:57:37 +0000

    fix: update default query options to disable refetching on window focus and reconnect

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -8,6 +8,9 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 0, // disable retries by default, we will enable it for specific queries
+      refetchOnWindowFocus: false, // disable refetching on window focus by default
+      refetchOnReconnect: false, // disable refetching on reconnect by default
+      refetchInterval: 5_000, // refetch every 5 seconds by default
     },
   },
 });
